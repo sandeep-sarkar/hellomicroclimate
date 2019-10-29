64bc56b2c5dffd2225e680128dcd298bbccc4ed3
Microclimate User <microclimate.user@localhost>
2019-10-29 16:36:30 +0530

    updated app.py

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def hello():
     return 'Hello World! \n'
 
+@app.route('/anotherhello')
+def anotherhello():
+    return 'Another Hello !! \n'
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
